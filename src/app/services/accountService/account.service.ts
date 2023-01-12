9321c74a8f5f1d6729d22e529f2bd7320c123884
daniel <daniel97.le@gmail.com>
2023-01-11 22:36:14 -0700

    1

--- src/app/services/accountService/account.service.ts
+++ src/app/services/accountService/account.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
 import { api } from '../../lib/utils/axios';
-import { Account } from '../../models/account';
+import { Account } from '../../models/account/account';
 import {logger} from '../../lib/utils/logger'
 
 @Injectable({
   providedIn: 'root'
 })
-class AccountService {
+export class AccountService {
   async get(){{
     const response = await api.get<Account>('/example')
     logger.log(response.data)
   }}
 }
 
-export const accountService = new AccountService()
